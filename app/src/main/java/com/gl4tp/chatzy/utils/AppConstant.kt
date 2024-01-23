ca37aa21109f16696a4ccca54131905d69de4a41
Nour Ben Hajla <92574404+Nourbh17@users.noreply.github.com>
2024-01-23 23:36:47 +0100

    Update AppConstant.kt

--- app/src/main/java/com/gl4tp/chatzy/utils/AppConstant.kt
+++ app/src/main/java/com/gl4tp/chatzy/utils/AppConstant.kt
@@ -2,6 +2,6 @@ package com.gl4tp.chatzy.utils
 
 const val BASE_URL = "https://api.openai.com/v1/"
 const val CHAT_GPT_MODEL ="gpt-3.5-turbo"
-const val OPENAI_API_KEY ="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"
+const val OPENAI_API_KEY ="API_Key_Here"
 
 
